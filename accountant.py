ecc949d03ce0f19f7058b921f5cb80594d4e77c8
fachu000 <fachu000@gmail.com>
2018-04-08 14:35:01 +0200

    Starting repository

---
+++ accountant.py
@@ -0,0 +1,32 @@
+#  ACCOUNTANT 1.0
+#
+# PROGRAM TO KEEP PERSONAL ACCOUNTS
+#
+# NEXT STEPS:
+# 
+# - Read files
+# - Plot total vs time
+# - Classify each transaction in categories
+# - Plot expended money vs time for each category
+#
+
+
+# For Python + GTK, see:  https://python-gtk-3-tutorial.readthedocs.io/en/latest/introduction.html
+
+########################################################################
+
+
+########################################################################
+
+from datetime import datetime,timedelta,time,date
+
+###############################
+
+print('here we go')
+
+
+quit()
+
+
+
+
