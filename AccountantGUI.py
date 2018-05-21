465c5e9d31a8c7101d71bf367834816a64bb65ff
fachu000 <fachu000@gmail.com>
2018-05-21 10:11:56 +0200

    Buttons for loading and saving lists of transactions

---
+++ AccountantGUI.py
@@ -0,0 +1,285 @@
+
+
+from Transaction import Transaction
+
+
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class AccountantGUI(Gtk.Window):
+
+    def __init__(self,l_transactions):
+
+        print('peixe')
+        Gtk.Window.__init__(self, title="Accountant GUI")
+        self.set_border_width(10)
+        self.l_transactions = l_transactions
+
+        #Setting up the self.grid in which the elements are to be positioned
+        self.grid = Gtk.Grid()
+        self.grid.set_column_homogeneous(True)
+        self.grid.set_row_homogeneous(True)
+        self.add(self.grid)
+
+        # List store for transactions
+        self.transaction_liststore = Gtk.ListStore(str, str, str,str,str,str,str)
+        self.fillTransactionListStore()
+
+        # Treeview for transactions
+        self.transaction_treeview =   Gtk.TreeView.new_with_model(self.transaction_liststore)
+        self.select = self.transaction_treeview.get_selection()
+        self.selectedTransactionTreeIter = None
+        self.select.connect("changed", self.on_tree_selection_changed)
+
+        for i, column_title in enumerate(["Date", "Amount","Category", "Description","Account","Purchase date","Interest date"]):
+            renderer = Gtk.CellRendererText()
+            column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+            self.transaction_treeview.append_column(column)
+
+        # # # List store for monthly hours
+        # self.formMonthlyHours_liststore()
+
+        # # Treeview for monthly hours
+        # self.monthlyHours_treeview =   Gtk.TreeView.new_with_model(self.monthlyHours_liststore)
+
+        # for i, column_title in enumerate(['Month']+Calendar.lstr_projectLabels + Calendar.lstr_genericProjectLabels + ['Total']):
+        #     renderer = Gtk.CellRendererText()
+        #     column = Gtk.TreeViewColumn(column_title, renderer, text=i)
+        #     self.monthlyHours_treeview.append_column(column)
+
+        # #creating buttons to assign events to projects
+        # self.l_projectAssignmentButtons = list()
+        # for projectLabel in Calendar.lstr_projectLabels:
+        #     button = Gtk.Button(projectLabel)
+        #     self.l_projectAssignmentButtons.append(button)
+        #     button.connect("clicked",
+        #     self.projectAssignmentButtonCallback)
+
+        # #creating buttons in top row
+        saveButton = Gtk.Button('Save')
+        saveButton.connect("clicked", self.saveButtonCallback)
+        loadButton = Gtk.Button('Load')
+        loadButton.connect("clicked", self.loadButtonCallback)
+        appendFromCSVFileButton = Gtk.Button('Append From CSV')
+        appendFromCSVFileButton.connect("clicked", self.appendFromCSVFileButtonCallback)
+        autoAssignButton = Gtk.Button('Auto Assign')
+        autoAssignButton.connect("clicked", self.autoAssignButtonCallback)
+        printAssignmentsButton = Gtk.Button('Print Assignments')
+        printAssignmentsButton.connect("clicked", self.printAssignmentsButtonCallback)
+
+
+        #setting up the layout, putting the treeview in a scrollwindow, and the buttons in a row
+        self.transaction_scrollableTreelist = Gtk.ScrolledWindow()
+        self.transaction_scrollableTreelist.set_vexpand(True)
+        # self.monthlyHours_scrollableTreelist = Gtk.ScrolledWindow()
+        # self.monthlyHours_scrollableTreelist.set_vexpand(True)
+
+
+        # Place widgets in the grid
+        self.grid.attach( saveButton , 0, 0, 2, 1)
+        self.grid.attach( loadButton , 2, 0, 2, 1)
+        self.grid.attach( appendFromCSVFileButton , 4, 0, 2, 1)
+        self.grid.attach( autoAssignButton , 6, 0, 2, 1)
+        #self.grid.attach( printAssignmentsButton , 8, 0, 2, 1)
+        self.grid.attach(self.transaction_scrollableTreelist, 0, 1, 12, 10)
+#        self.grid.attach_next_to(self.l_projectAssignmentButtons[0], self.transaction_scrollableTreelist, Gtk.PositionType.BOTTOM, 1, 1)
+        # for i, button in enumerate(self.l_projectAssignmentButtons[1:]):
+        #     self.grid.attach_next_to(button, self.l_projectAssignmentButtons[i], Gtk.PositionType.RIGHT, 1, 1)
+        # self.grid.attach(self.monthlyHours_scrollableTreelist, 0, 12, 12, 10)
+        self.transaction_scrollableTreelist.add(self.transaction_treeview)
+#        self.monthlyHours_scrollableTreelist.add(self.monthlyHours_treeview)
+
+        self.show_all()
+        print('peixe')
+
+    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #
+    # FUNCTIONS TO MANIPULATE THE LIST STORE
+    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #
+
+    def fillTransactionListStore(self):
+        
+        for transactionIndex in range(0,len(self.l_transactions)):
+            transaction =  self.l_transactions[transactionIndex]
+            row = AccountantGUI.transactionToStoreRowList(transaction)
+            self.transaction_liststore.append(row)
+
+    def updateStoreRow(self,i_rowIndex):
+        """Updates the i_rowIndex-th row of the table"""
+
+        rowList = AccountantGUI.transactionToStoreRowList(self.l_transactions[i_rowIndex])
+
+        path = Gtk.TreePath.new_from_string(str(i_rowIndex))
+        treeIter = self.transaction_liststore.get_iter(path)
+        for colInd in range(0,len(rowList)):
+            self.transaction_liststore[treeIter][colInd] = rowList[colInd]
+                    
+    def transactionToStoreRowList(transaction):
+        """Returns a list of strings. The n-th entry is the text to be
+        displayed in the n-th column of the row corresponding to the
+        Transaction <transaction>"""
+        str_date = transaction.d_date.strftime('%Y/%m/%d')
+        if transaction.d_purchaseDate:
+            str_purchaseDate = transaction.d_purchaseDate.strftime('%Y/%m/%d')
+        else:
+            str_purchaseDate = ''
+        if transaction.d_interestDate:
+            str_interestDate = transaction.d_interestDate.strftime('%Y/%m/%d')
+        else:
+            str_interestDate = ''            
+        str_description = transaction.str_description
+        str_amount = str(transaction.f_amount)
+        str_category = transaction.str_category
+        str_account = transaction.str_account
+        return [str_date,str_amount,str_category,str_description,str_account,str_purchaseDate,str_interestDate]
+
+    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #
+    # CALLBACK FUNCTIONS
+    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #    # # # #
+
+    def saveButtonCallback(self,widget):
+
+        dialog = Gtk.FileChooserDialog("Please choose a .pk file", self,
+                                       Gtk.FileChooserAction.SAVE,
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("File selected: " + dialog.get_filename())
+            Transaction.saveTransactionList(self.l_transactions,dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+        print('now you save...')
+
+
+    def loadButtonCallback(self,widget):
+
+        dialog = Gtk.FileChooserDialog("Please choose a .pk file", self,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self.l_transactions = Transaction.loadTransactionList(dialog.get_filename())
+            self.transaction_liststore.clear()
+            self.fillTransactionListStore()
+
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+        dialog.destroy()
+
+
+    def appendFromCSVFileButtonCallback(self,widget):
+
+        dialog = DialogExample(self)
+        response = dialog.run()
+
+        if response == Gtk.ResponseType.OK:
+            print("Appending transactions from default CSV files.")
+            self.appendTransactionsFromDefaultCSVFiles()
+        elif response == Gtk.ResponseType.CANCEL:
+            print("The Cancel button was clicked")
+
+        dialog.destroy()
+
+        return
+        self.calendar.appendFromCSVFile()
+
+        # Update event table
+        for eventIndex in range(0,len(self.calendar.l_CalEvents)):
+            self.updateStoreRow(eventIndex)
+        # Update table of monthly events 
+        self.updateMonthlyHours_liststore()
+
+
+    def appendTransactionsFromDefaultCSVFiles(self):
+        
+        ll_files = [ ['../data/CSV/checking.csv', 'ISO-8859-1','CHECKING-NO' ],
+                     ['../data/CSV/savings.csv', 'ISO-8859-1','SAVINGS-NO' ],
+                     ['../data/CSV/creditCard.csv','UTF8','CREDIT_CARD-NO']]
+
+
+        #l_transactions = Transaction.readTransactionListFromCSVFile(ll_files)
+        l_transactions_CSV = Transaction.readTransactionListFromCSVFile(ll_files)
+
+        num_newTransactions = Transaction.combineListsOfTransactions(self.l_transactions,l_transactions_CSV)
+        print('new transactions = ',num_newTransactions)
+        self.transaction_liststore.clear()
+        self.fillTransactionListStore()
+
+
+
+
+    def autoAssignButtonCallback(self,widget):
+
+        self.calendar.autoAssignEvents()
+        # Update event table
+        for eventIndex in range(0,len(self.calendar.l_CalEvents)):
+            self.updateStoreRow(eventIndex)
+        # Update table of monthly events 
+        self.updateMonthlyHours_liststore()
+
+    def printAssignmentsButtonCallback(self,widget):
+
+        self.calendar.printAssignments() 
+
+
+    def on_tree_selection_changed(self,selection):
+        model, treeiter = selection.get_selected()
+        if treeiter is not None:
+            print("You selected", model[treeiter][0])        
+        self.selectedEventTreeIter = treeiter
+#        print('path = ',type(self.selectedEventTreeIter))
+
+
+    def projectAssignmentButtonCallback(self, widget):
+        """This function sets the property str_project of the selected
+        event to the value of the label of the button."""  
+
+        str_selectedProject = widget.get_label()
+        treeiter = self.selectedEventTreeIter
+        if treeiter == None:
+            return
+        selectedEventIndex = self.events_liststore.get_path(treeiter).get_indices()[0]
+        self.calendar.l_CalEvents[selectedEventIndex].str_project = str_selectedProject
+        self.calendar.l_CalEvents[selectedEventIndex].print()
+        self.updateStoreRow(selectedEventIndex)
+        #self.calendar.printMonthlyHoursPerProject()
+        self.updateMonthlyHours_liststore()
+
+
+#        self.events_liststore
+        
+
+#############################
+
+
+
+class DialogExample(Gtk.Dialog):
+
+    def __init__(self, parent):
+        Gtk.Dialog.__init__(self, "My Dialog", parent, 0,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OK, Gtk.ResponseType.OK))
+
+        self.set_default_size(150, 100)
+
+        label = Gtk.Label('Do you want to read the files checking.csv, savings.csv' + ' and creditCard.csv from the folder data/CSV?')
+
+        box = self.get_content_area()
+        box.add(label)
+        self.show_all()
+
+    def run(self):
+
+        response = Gtk.Dialog.run(self)
+        print('psssss')
+#        return (response,str_filename,str_format,str_account)
+        return response
--- accountant.py
+++ accountant.py
@@ -3,12 +3,11 @@
 # PROGRAM TO KEEP PERSONAL ACCOUNTS
 #
 # NEXT STEPS:
-# 
-# - Create class transactions
-# - Read files -> form list of Transactions that contains the transactions of all  accounts.
-# - Save/load list of Transactions to a file
-# - Function that combines the loaded list of Transactions with the list read from a file. The list is sorted by date at the end.
-# - Graphical interface
+# - add buttons to load/save and link them to the corresponding functions
+# - add functionality to add comments to a transaction
+# - Automatic classification of transactions into categories. If a transaction has the same description field as an already classified transaction, then it is assigned to the same category. If there already exist multiple classified transactions with the same description and different category, then the transaction at hand should not be automatically classified. 
+#
+# GRAPHICAL INTERFACE:
 #     -On top: buttons to load/save Transaction lists or to read files
 #     -Below: text boxes to specify filters (which transactions to display)
 #     -Below: list of transactions that pass the filters
@@ -16,7 +15,6 @@
 #     -Below: buttons to plot, e.g. 
         # - balance vs time
         # - the amount of the filtered transactions vs time
-# - Automatic classification of transactions into categories. If a transaction has the same description field as an already classified transaction, then it is assigned to the same category. If there already exist multiple classified transactions with the same description and different category, then the transaction at hand should not be automatically classified. 
 #
 
 
@@ -26,161 +24,43 @@
 
 
 ########################################################################
-
-class Transaction:
-    """An object of this class describes a bank transaction."""
-
-    def __init__(self):
-        self.d_date = []
-        self.str_description = ''
-        self.d_interestDate = []
-        self.d_purchaseDate = []
-        self.f_amount = 0 # >0 means incoming to the account
-        self.str_fileName = ''
-
-    def readTransactionFromString(str_csv,str_format,str_fileName):
-#        return Transaction.readTransactionFromStringCreditCardFormat(str_csv)
-        str1 = 'Date;Description;Interest date;In;Out;'
-        str2 = ' "DATE";"DESCRIPTION";"PURCHASE DATE";"AMOUNT";'
-
-        if str_format == str1:
-            trans =  Transaction.readTransactionFromStringStandardFormat(str_csv)
-        elif str_format[1:-1] == str2[1:-1]:
-            trans =  Transaction.readTransactionFromStringCreditCardFormat(str_csv)
-        else:
-            print('ERROR: invalid format')
-            print('format:"',str_format,'"')
-            print('format:"',str2,'"')
-            quit()
-
-        if trans:
-            trans.str_fileName = str_fileName
-
-        return trans
+    
         
-
-    def readTransactionFromStringStandardFormat(str_csv):
-        tran = Transaction()
-        lstr_fields = str_csv.split(';')
-
-        # date
-        str_date = lstr_fields[0] 
-        if len(str_date)>0:
-            tran.d_date = Transaction.dateStrToDate(str_date)
-
-        # description
-        tran.str_description = lstr_fields[1]
-
-        # interest date
-        str_interestDate = lstr_fields[2] 
-        if len(str_interestDate)>0:
-            tran.d_interestDate = Transaction.dateStrToDate(str_interestDate)
-
-        # Amount in 
-        str_inAmount = lstr_fields[3].replace(',','.')
-        if str_inAmount:
-            f_in = float( str_inAmount )
-        else:
-            f_in = 0
-
-        # Amount out 
-        str_outAmount = lstr_fields[4].replace(',','.')
-        if str_outAmount:
-            f_out = float( str_outAmount )
-        else:
-            f_out = 0
-
-        tran.f_amount = f_in + f_out
-        
-        return tran
-
-    def readTransactionFromStringCreditCardFormat(str_csv):
-        if len(str_csv)<5:
-            return []
-        elif str_csv[0:len('"TOTAL"')] == '"TOTAL"':
-            return []
-        
-
-        tran = Transaction()
-
-        lstr_fields = str_csv.split(';')
-
-        # date
-        str_date = lstr_fields[0].split('"')[1]
-        if len(str_date)>0:
-            tran.d_date = Transaction.dateStrToDate(str_date)
-
-        # description
-        tran.str_description = lstr_fields[1].split('"')[1]
-
-        # Purchase date
-        str_purchaseDate = lstr_fields[2].split('"')[1]
-        if len(str_purchaseDate)>0:
-            tran.d_purchaseDate = Transaction.dateStrToDate(str_purchaseDate)
-
-        # Amount out 
-        str_outAmount = lstr_fields[3].split('"')[1].replace(',','.')
-        if str_outAmount:
-            tran.f_amount = float( str_outAmount )
-        else:
-            tran.f_amount = 0
-
-        return tran
-
-
-
-
-    def dateStrToDate(str_date):
-
-        if not str_date[0].isdigit():
-            print('str_date = ',str_date)
-            print('ERROR: invalid format of time field')
-            quit()
-        dt_out = date(int(str_date[6:10]),int(str_date[3:5]),int(str_date[0:2]))
-        return dt_out
-
-    def print(self):
-        print('------------------------------')
-        print('DATE: ',self.d_date)
-        print('DESCRIPTION: ',self.str_description)
-        print('INTEREST DATE: ',self.d_interestDate)
-        print('PURCHASE DATE: ',self.d_purchaseDate)
-        print('AMOUNT: ',self.f_amount)
-        print('FILE NAME: ',self.str_fileName)
-        print('------------------------------')
-
-
 ########################################################################
 
-from datetime import datetime,timedelta,time,date
+
+from AccountantGUI import AccountantGUI
+
+from Transaction import Transaction
 
 ###############################
 
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+# win = Gtk.Window()
+# win.connect("destroy", Gtk.main_quit)
+# win.show_all()
+# Gtk.main()
+
+# quit()
+
+
 print('here we go')
 
-str_transactionFileName = '../data/savings.csv'
-#str_transactionFileName = '../data/creditCard.csv'
-
-ll_files = [ ['../data/checking.csv', 'ISO-8859-1' ],
-             ['../data/savings.csv', 'ISO-8859-1' ],
-            ['../data/creditCard.csv','UTF8']]
-
-l_transactions = [];
-
-# read file
-for l_file in ll_files:
-    with open(l_file[0], 'r',encoding = l_file[1]) as input:
-    #with open(str_transactionFileName, 'r',encoding = 'UTF8') as input:
-
-        str_format = input.readline()[0:-1] # first line neglected
-
-        for myline in input:
-            tran = Transaction.readTransactionFromString(myline,str_format,l_file[0])
-            if tran:
-                l_transactions.append(tran)
 
 
-for trans in l_transactions:
-    trans.print()
+#win = AccountantGUI(l_transactions)
+win = AccountantGUI([])
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
+quit()
+
+#l_transactions = Transaction.sortTransactionList(l_transactions)
+
+# for trans in l_transactions:
+#     trans.print()
 
 quit()
